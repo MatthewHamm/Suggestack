f6539a2604975f1f872c799e3496dc9aac3afbc6
Matthew Hamm <matthewhamm00@gmail.com>
2021-12-03 22:11:36 +0000

    Background tidied up

--- src/bg/background.js
+++ src/bg/background.js
@@ -28,187 +28,90 @@
                 fetch("https://www.startpage.com/sp/search?query="+encodeURIComponent(request.q.join(" OR ")+" site:"+relstacks.slice(0,64).join(" OR site:")))
                 .then(response=>response.text())
                 .then(function(data){
-                  
-                  resultsdata=String(data);
-                  console.log(resultsdata);
-                  resultarr=resultsdata.split("div");
-                  var resultlist=resultarr.filter(function(item){
-                    return item.includes("w-gl__result-title result-link")
-                  });
-                  var descrlist=resultarr.filter(function(item){
-                    return item.includes("w-gl__description")
-                  });
-                  
+                  var resultTable=startpageParse(data);
                   var results=[];
-                  var links=[]; 
+                  var links=[];
                   var desc=[];
-                
-                  resultlist.forEach(getText);
-                  resultlist.forEach(getlinks);
-                  descrlist.splice(1).forEach(getDesc);
-                  console.log(desc);
+                  resultTable.forEach(getRe);                  
+                  
+                  function getRe(item){
+                    results.push(item.title);
+                    links.push(item.link);
+                    desc.push(item.desc);
+                  }
                   if(results.length<5){
                     fetch("https://www.startpage.com/sp/search?query="+encodeURIComponent("site:substack.com/p/ "+request.q.join(" OR ")))
                     .then(response=>response.text())
                     .then(function(data){
-                  
-                      resultsdata=String(data);
-                      console.log(resultsdata);
-                      resultarr=resultsdata.split("div");
-                      var resultlist=resultarr.filter(function(item){
-                        return item.includes("w-gl__result-title result-link")
-                      });
-                      var descrlist=resultarr.filter(function(item){
-                        return item.includes("w-gl__description")
-                      });
                       
-                      var results=[];
-                      var links=[]; 
-                      var desc=[];
-                    
-                      resultlist.forEach(getText);
-                      resultlist.forEach(getlinks);
-                      descrlist.splice(1).forEach(getDesc);
-                      console.log(desc);
+                      resultTable=startpageParse(data);
+                      resultTable.forEach(getRe);                  
+                  
+                      function getRe(item){
+                        results.push(item.title);
+                        links.push(item.link);
+                        desc.push(item.desc);
+                      }                      
                       fetch("https://api.qwant.com/v3/search/web?q="+encodeURIComponent("site:substack.com/p/ "+request.q[0])+"&count=10&locale=en_GB&offset=0&device=desktop&safesearch=0")
                       .then(response=>response.json())
                       .then(function(data){
-                        console.log(data.data.result.items.mainline);
-                        let webresult=data.data.result.items.mainline.filter(result=>result.type=="web");
-                        console.log(webresult);
-                        for(i in webresult){
-                          console.log(webresult[i].items);
-                          webresult[i].items.forEach(getRe);
-                        }
-                        
+
+                        resultTable.forEach(getRe);
+                        sendResponse(qwantParse(data,results,links,desc));
                         function getRe(item){
                           results.push(item.title);
-                          links.push(item.url);
+                          links.push(item.link);
                           desc.push(item.desc);
                         }
-                        var resultTable=results.map(title=>{
-                          return{
-                            "title":title,
-                            "desc":desc[results.indexOf(title)],
-                            "link":links[results.indexOf(title)]
-                          }
-                        })
-                        console.log(resultTable);
-                        sendResponse(resultTable);
                       })
                       
-                    function getText(html){
-                
-                      results.push(html.split("h3")[1].replace("</","").replace(">","").trim());
-                    }
-                    function getlinks(html){
-                      links.push(html.split("href=")[1].split('"')[1])
-                    }
-                    function getDesc(html){
-                      desc.push(html.replace("<p class=\"w-gl__description\">","").replace("</p","").replace("</","").replace(">","").replace("<b>","").replace("b>","").trim());
-                    }
+                    
                     })
                   }
                   else{
                     fetch("https://api.qwant.com/v3/search/web?q="+encodeURIComponent("site:substack.com/p/ "+request.q[0])+"&count=10&locale=en_GB&offset=0&device=desktop&safesearch=0")
                     .then(response=>response.json())
                     .then(function(data){
-                      console.log(data.data.result.items.mainline);
-                      let webresult=data.data.result.items.mainline.filter(result=>result.type=="web");
-                      console.log(webresult);
-                      for(i in webresult){
-                        console.log(webresult[i].items);
-                        webresult[i].items.forEach(getRe);
-                      }
-                      
+                      resultTable.forEach(getRe);
+                      sendResponse(qwantParse(data,results,links,desc));
                       function getRe(item){
                         results.push(item.title);
-                        links.push(item.url);
+                        links.push(item.link);
                         desc.push(item.desc);
                       }
-                      var resultTable=results.map(title=>{
-                        return{
-                          "title":title,
-                          "desc":desc[results.indexOf(title)],
-                          "link":links[results.indexOf(title)]
-                        }
-                      })
-                      console.log(resultTable);
-                      sendResponse(resultTable);
                   })
                   }
-                  function getText(html){
                   
-                    results.push(html.split("h3")[1].replace("</","").replace(">","").trim());
-                  }
-                  function getlinks(html){
-                    links.push(html.split("href=")[1].split('"')[1])
-                  }
-                  function getDesc(html){
-                    desc.push(html.replace("<p class=\"w-gl__description\">","").replace("</p","").replace("</","").replace(">","").replace("<b>","").replace("b>","").trim());
-                  }
                 })
                 
               }else{
                 fetch("https://www.startpage.com/sp/search?query="+encodeURIComponent("site:substack.com/p/ "+request.q.join(" OR ")))
                 .then(response=>response.text())
                 .then(function(data){
-                  
-                  resultsdata=String(data);
-                  console.log(resultsdata);
-                  resultarr=resultsdata.split("div");
-                  var resultlist=resultarr.filter(function(item){
-                    return item.includes("w-gl__result-title result-link")
-                  });
-                  var descrlist=resultarr.filter(function(item){
-                    return item.includes("w-gl__description")
-                  });
-                  
+                  var resultTable=startpageParse(data);
                   var results=[];
-                  var links=[]; 
+                  var links=[];
                   var desc=[];
-                
-                  resultlist.forEach(getText);
-                  resultlist.forEach(getlinks);
-                  descrlist.splice(1).forEach(getDesc);
-                  console.log(desc);
+                  resultTable.forEach(getRe);                  
+                  
+                  function getRe(item){
+                    results.push(item.title);
+                    links.push(item.link);
+                    desc.push(item.desc);
+                  }
                   fetch("https://api.qwant.com/v3/search/web?q="+encodeURIComponent("site:substack.com/p/ "+request.q[0])+"&count=10&locale=en_GB&offset=0&device=desktop&safesearch=0")
                   .then(response=>response.json())
                   .then(function(data){
-                    console.log(data.data.result.items.mainline);
-                    let webresult=data.data.result.items.mainline.filter(result=>result.type=="web");
-                    console.log(webresult);
-                    for(i in webresult){
-                      console.log(webresult[i].items);
-                      webresult[i].items.forEach(getRe);
-                    }
                     
+                    resultTable.forEach(getRe);
+                    sendResponse(qwantParse(data,results,links,desc));
                     function getRe(item){
                       results.push(item.title);
-                      links.push(item.url);
+                      links.push(item.link);
                       desc.push(item.desc);
                     }
-                    var resultTable=results.map(title=>{
-                      return{
-                        "title":title,
-                        "desc":desc[results.indexOf(title)],
-                        "link":links[results.indexOf(title)]
-                      }
-                    })
-                    console.log(resultTable);
-                    sendResponse(resultTable);
                   })
                   
-                function getText(html){
-                  
-                  results.push(html.split("h3")[1].replace("</","").replace(">","").trim());
-                }
-                function getlinks(html){
-                  links.push(html.split("href=")[1].split('"')[1])
-                }
-                function getDesc(html){
-                  desc.push(html.replace("<p class=\"w-gl__description\">","").replace("</p","").replace("</","").replace(">","").replace("<b>","").replace("b>","").trim());
-                }
                 })
 
               }
@@ -219,62 +122,31 @@
             fetch("https://www.startpage.com/sp/search?query="+encodeURIComponent("site:substack.com/p/ "+request.q.join(" OR ")))
             .then(response=>response.text())
             .then(function(data){
-              
-              resultsdata=String(data);
-              console.log(resultsdata);
-              resultarr=resultsdata.split("div");
-              var resultlist=resultarr.filter(function(item){
-                return item.includes("w-gl__result-title result-link")
-              });
-              var descrlist=resultarr.filter(function(item){
-                return item.includes("w-gl__description")
-              });
-              
+              var resultTable=startpageParse(data);
               var results=[];
-              var links=[]; 
+              var links=[];
               var desc=[];
-            
-              resultlist.forEach(getText);
-              resultlist.forEach(getlinks);
-              descrlist.splice(1).forEach(getDesc);
-              console.log(desc);
+              resultTable.forEach(getRe);                  
+              
+              function getRe(item){
+                results.push(item.title);
+                links.push(item.link);
+                desc.push(item.desc);
+              }
               fetch("https://api.qwant.com/v3/search/web?q="+encodeURIComponent("site:substack.com/p/ "+request.q[0])+"&count=10&locale=en_GB&offset=0&device=desktop&safesearch=0")
               .then(response=>response.json())
               .then(function(data){
-                console.log(data.data.result.items.mainline);
-                let webresult=data.data.result.items.mainline.filter(result=>result.type=="web");
-                console.log(webresult);
-                for(i in webresult){
-                  console.log(webresult[i].items);
-                  webresult[i].items.forEach(getRe);
-                }
-                
+
+                resultTable.forEach(getRe);
+                sendResponse(qwantParse(data,results,links,desc));
                 function getRe(item){
                   results.push(item.title);
-                  links.push(item.url);
+                  links.push(item.link);
                   desc.push(item.desc);
                 }
-                var resultTable=results.map(title=>{
-                  return{
-                    "title":title,
-                    "desc":desc[results.indexOf(title)],
-                    "link":links[results.indexOf(title)]
-                  }
-                })
-                console.log(resultTable);
-                sendResponse(resultTable);
               })
               
-            function getText(html){
-                
-              results.push(html.split("h3")[1].replace("</","").replace(">","").trim());
-            }
-            function getlinks(html){
-              links.push(html.split("href=")[1].split('"')[1])
-            }
-            function getDesc(html){
-              desc.push(html.replace("<p class=\"w-gl__description\">","").replace("</p","").replace("</","").replace(">","").replace("<b>","").replace("b>","").trim());
-            }
+            
             })
             }
           
@@ -292,3 +164,67 @@
       }
   }
 );
+function startpageParse(datal){
+  resultsdata=String(datal);
+  console.log(resultsdata);
+  resultarr=resultsdata.split("div");
+  var resultlist=resultarr.filter(function(item){
+    return item.includes("w-gl__result-title result-link")
+  });
+  var descrlist=resultarr.filter(function(item){
+    return item.includes("w-gl__description")
+  });
+  let results = [];
+  let links=[];
+  let desc=[];
+
+  resultlist.forEach(getText);
+  resultlist.forEach(getlinks);
+  descrlist.splice(1).forEach(getDesc);
+  var resultTable=results.map(title=>{
+    return{
+      "title":title,
+      "desc":desc[results.indexOf(title)],
+      "link":links[results.indexOf(title)]
+    }
+  })
+  return resultTable
+  function getText(html){
+                
+    results.push(html.split("h3")[1].replace("</","").replace(">","").trim());
+  }
+  function getlinks(html){
+    links.push(html.split("href=")[1].split('"')[1])
+  }
+  function getDesc(html){
+    desc.push(html.replace("<p class=\"w-gl__description\">","").replace("</p","").replace("</","").replace(">","").replace("<b>","").replace("b>","").trim());
+  }
+}
+function qwantParse(datal,resultsl,linksl,descl){
+  console.log(resultsl);
+  let nresults = resultsl;
+  let nlinks=linksl;
+  let ndesc=descl;
+  console.log(datal.data.result.items.mainline);
+  let webresult=datal.data.result.items.mainline.filter(result=>result.type=="web");
+  console.log(webresult);
+  for(i in webresult){
+    console.log(webresult[i].items);
+    webresult[i].items.forEach(getRe);
+  }
+  
+  function getRe(item){
+    nresults.push(item.title);
+    nlinks.push(item.url);
+    ndesc.push(item.desc);
+  }
+  var resultTable=nresults.map(title=>{
+    return{
+      "title":title,
+      "desc":ndesc[nresults.indexOf(title)],
+      "link":nlinks[nresults.indexOf(title)]
+    }
+  })
+  console.log(resultTable);
+  return resultTable;
+}
