ddcd30eb910bf89b7510071517ebe3aea6d13d6d
Matthew Hamm <matthewhamm00@gmail.com>
2021-12-03 19:12:32 +0000

    migrated to manifets v3, so had to remove domparser

--- src/bg/background.js
+++ src/bg/background.js
@@ -31,34 +31,44 @@
                   
                   resultsdata=String(data);
                   console.log(resultsdata);
-                  var docstart=new DOMParser().parseFromString(resultsdata, "text/html");
-                  var resultlist=[...docstart.getElementsByClassName("w-gl__result-title result-link")];
-                  var descrlist=[...docstart.getElementsByClassName("w-gl__description")];
+                  resultarr=resultsdata.split("div");
+                  var resultlist=resultarr.filter(function(item){
+                    return item.includes("w-gl__result-title result-link")
+                  });
+                  var descrlist=resultarr.filter(function(item){
+                    return item.includes("w-gl__description")
+                  });
+                  
                   var results=[];
                   var links=[]; 
                   var desc=[];
                 
                   resultlist.forEach(getText);
                   resultlist.forEach(getlinks);
-                  descrlist.forEach(getDesc);
+                  descrlist.splice(1).forEach(getDesc);
                   if(results.length<5){
                     fetch("https://www.startpage.com/sp/search?query="+encodeURIComponent("site:substack.com/p/ "+request.q.join(" OR ")))
                     .then(response=>response.text())
                     .then(function(data){
                   
-                    resultsdata=String(data);
-                    console.log(resultsdata);
-                    var docstart=new DOMParser().parseFromString(resultsdata, "text/html");
-                    var resultlist=[...docstart.getElementsByClassName("w-gl__result-title result-link")];
-                    var descrlist=[...docstart.getElementsByClassName("w-gl__description")];
-                    var results=[];
-                    var links=[]; 
-                    var desc=[];
-                  
-                    resultlist.forEach(getText);
-                    resultlist.forEach(getlinks);
-                    descrlist.forEach(getDesc);
-                    fetch("https://api.qwant.com/v3/search/web?q="+encodeURIComponent("site:substack.com/p/ "+request.q[0])+"&count=10&locale=en_GB&offset=0&device=desktop&safesearch=0")
+                      resultsdata=String(data);
+                      console.log(resultsdata);
+                      resultarr=resultsdata.split("div");
+                      var resultlist=resultarr.filter(function(item){
+                        return item.includes("w-gl__result-title result-link")
+                      });
+                      var descrlist=resultarr.filter(function(item){
+                        return item.includes("w-gl__description")
+                      });
+                      
+                      var results=[];
+                      var links=[]; 
+                      var desc=[];
+                    
+                      resultlist.forEach(getText);
+                      resultlist.forEach(getlinks);
+                      descrlist.splice(1).forEach(getDesc);
+                      fetch("https://api.qwant.com/v3/search/web?q="+encodeURIComponent("site:substack.com/p/ "+request.q[0])+"&count=10&locale=en_GB&offset=0&device=desktop&safesearch=0")
                       .then(response=>response.json())
                       .then(function(data){
                         console.log(data.data.result.items.mainline);
@@ -86,54 +96,54 @@
                       })
                       
                     function getText(html){
-                      
-                      results.push(html.innerText.trim());
+                
+                      results.push(html.split("h3")[1].replace("</","").replace(">","").trim());
                     }
                     function getlinks(html){
-                      links.push(html.href)
+                      links.push(html.split("href=")[1].split('"')[1])
                     }
                     function getDesc(html){
-                      desc.push(html.innerText.trim())
+                      desc.push(html.replace("<p class=w-gl__description>","").replace("</p>","").replace("</","").replace(">","").replace("<b>","").replace("b>","").trim());
                     }
                     })
                   }
                   else{
                     fetch("https://api.qwant.com/v3/search/web?q="+encodeURIComponent("site:substack.com/p/ "+request.q[0])+"&count=10&locale=en_GB&offset=0&device=desktop&safesearch=0")
-                  .then(response=>response.json())
-                  .then(function(data){
-                    console.log(data.data.result.items.mainline);
-                    let webresult=data.data.result.items.mainline.filter(result=>result.type=="web");
-                    console.log(webresult);
-                    for(i in webresult){
-                      console.log(webresult[i].items);
-                      webresult[i].items.forEach(getRe);
-                    }
-                    
-                    function getRe(item){
-                      results.push(item.title);
-                      links.push(item.url);
-                      desc.push(item.desc);
-                    }
-                    var resultTable=results.map(title=>{
-                      return{
-                        "title":title,
-                        "desc":desc[results.indexOf(title)],
-                        "link":links[results.indexOf(title)]
+                    .then(response=>response.json())
+                    .then(function(data){
+                      console.log(data.data.result.items.mainline);
+                      let webresult=data.data.result.items.mainline.filter(result=>result.type=="web");
+                      console.log(webresult);
+                      for(i in webresult){
+                        console.log(webresult[i].items);
+                        webresult[i].items.forEach(getRe);
                       }
-                    })
-                    console.log(resultTable);
-                    sendResponse(resultTable);
+                      
+                      function getRe(item){
+                        results.push(item.title);
+                        links.push(item.url);
+                        desc.push(item.desc);
+                      }
+                      var resultTable=results.map(title=>{
+                        return{
+                          "title":title,
+                          "desc":desc[results.indexOf(title)],
+                          "link":links[results.indexOf(title)]
+                        }
+                      })
+                      console.log(resultTable);
+                      sendResponse(resultTable);
                   })
                   }
                   function getText(html){
-                      
-                    results.push(html.innerText.trim());
+                  
+                    results.push(html.split("h3")[1].replace("</","").replace(">","").trim());
                   }
                   function getlinks(html){
-                    links.push(html.href)
+                    links.push(html.split("href=")[1].split('"')[1])
                   }
                   function getDesc(html){
-                    desc.push(html.innerText.trim())
+                    desc.push(html.replace("<p class=w-gl__description>","").replace("</p>","").replace("</","").replace(">","").replace("<b>","").replace("b>","").trim());
                   }
                 })
                 
@@ -144,16 +154,21 @@
                   
                   resultsdata=String(data);
                   console.log(resultsdata);
-                  var docstart=new DOMParser().parseFromString(resultsdata, "text/html");
-                  var resultlist=[...docstart.getElementsByClassName("w-gl__result-title result-link")];
-                  var descrlist=[...docstart.getElementsByClassName("w-gl__description")];
+                  resultarr=resultsdata.split("div");
+                  var resultlist=resultarr.filter(function(item){
+                    return item.includes("w-gl__result-title result-link")
+                  });
+                  var descrlist=resultarr.filter(function(item){
+                    return item.includes("w-gl__description")
+                  });
+                  
                   var results=[];
                   var links=[]; 
                   var desc=[];
                 
                   resultlist.forEach(getText);
                   resultlist.forEach(getlinks);
-                  descrlist.forEach(getDesc);
+                  descrlist.splice(1).forEach(getDesc);
                   
                   fetch("https://api.qwant.com/v3/search/web?q="+encodeURIComponent("site:substack.com/p/ "+request.q[0])+"&count=10&locale=en_GB&offset=0&device=desktop&safesearch=0")
                   .then(response=>response.json())
@@ -184,13 +199,13 @@
                   
                 function getText(html){
                   
-                  results.push(html.innerText.trim());
+                  results.push(html.split("h3")[1].replace("</","").replace(">","").trim());
                 }
                 function getlinks(html){
-                  links.push(html.href)
+                  links.push(html.split("href=")[1].split('"')[1])
                 }
                 function getDesc(html){
-                  desc.push(html.innerText.trim())
+                  desc.push(html.replace("<p class=w-gl__description>","").replace("</p>","").replace("</","").replace(">","").replace("<b>","").replace("b>","").trim());
                 }
                 })
 
@@ -205,16 +220,21 @@
               
               resultsdata=String(data);
               console.log(resultsdata);
-              var docstart=new DOMParser().parseFromString(resultsdata, "text/html");
-              var resultlist=[...docstart.getElementsByClassName("w-gl__result-title result-link")];
-              var descrlist=[...docstart.getElementsByClassName("w-gl__description")];
+              resultarr=resultsdata.split("div");
+              var resultlist=resultarr.filter(function(item){
+                return item.includes("w-gl__result-title result-link")
+              });
+              var descrlist=resultarr.filter(function(item){
+                return item.includes("w-gl__description")
+              });
+              
               var results=[];
               var links=[]; 
               var desc=[];
             
               resultlist.forEach(getText);
               resultlist.forEach(getlinks);
-              descrlist.forEach(getDesc);
+              descrlist.splice(1).forEach(getDesc);
               
               fetch("https://api.qwant.com/v3/search/web?q="+encodeURIComponent("site:substack.com/p/ "+request.q[0])+"&count=10&locale=en_GB&offset=0&device=desktop&safesearch=0")
               .then(response=>response.json())
@@ -244,14 +264,14 @@
               })
               
             function getText(html){
-              
-              results.push(html.innerText.trim());
+                
+              results.push(html.split("h3")[1].replace("</","").replace(">","").trim());
             }
             function getlinks(html){
-              links.push(html.href)
+              links.push(html.split("href=")[1].split('"')[1])
             }
             function getDesc(html){
-              desc.push(html.innerText.trim())
+              desc.push(html.replace("<p class=w-gl__description>","").replace("</p>","").replace("</","").replace(">","").replace("<b>","").replace("b>","").trim());
             }
             })
             }
